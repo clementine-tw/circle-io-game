8d6ca4b9d81a483c3e90c1ffd3b89f9689bfb882
ClemGamer <tsaiyouchen@gmail.com>
2025-08-04 14:13:08 +0800

    fix(server): fix staticcheck warning - error strings should not be capitalized (ST1005)

--- server/internal/auth/auth.go
+++ server/internal/auth/auth.go
@@ -8,7 +8,7 @@ import (
 func GetUrlQueryToken(r *http.Request) (string, error) {
 	token := r.URL.Query().Get("token")
 	if token == "" {
-		return "", errors.New("Token not found in URL query")
+		return "", errors.New("token not found in URL query")
 	}
 	return token, nil
 }
